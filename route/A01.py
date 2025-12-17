6c47eaf1b2f81b07a6554e13d6cb849d69ec53eb
SWGO-1819 <tonytony1714@gmail.com>
2025-12-17 19:59:30 +0900

    ignore파일 추가

--- route/A01.py
+++ route/A01.py
@@ -1,7 +1,7 @@
 import json
 import os
 from flask import Blueprint, render_template, current_app
-
+from flask import request
 # 'a01' 블루프린트를 생성합니다. 
 # url_prefix='/A01' 설정으로 이 블루프린트 내의 모든 라우트는 '/A01'을 기본 경로로 가집니다.
 a01_bp = Blueprint('a01', __name__, url_prefix='/A01')
@@ -28,6 +28,19 @@ def a01_index():
 @a01_bp.route('/cwe-22')
 def cwe_22():
     """
-    CWE-22 (Improper Authorization) 예시 페이지를 보여줍니다.
+    CWE-22 (Path Traversal) 예시 페이지를 보여줍니다.
     """
-    return render_template('A01/CWE-22.html')
+    DATA_PATH = "./profiles"
+    username = request.args.get("user")  # 사용자 입력
+    profile_data = []
+
+    # username이 있을 경우에만 파일을 읽도록 처리
+    if username:
+        profile_path = os.path.join(DATA_PATH, username)  # ❌ 검증 없이 경로 생성
+        try:
+            with open(profile_path, "r") as f:
+                profile_data = [line.strip() for line in f]  # 파일 내용을 한 줄씩 읽어 리스트에 저장
+        except Exception as e:
+            profile_data.append(f"Error: {e}")
+
+    return render_template(f'A01/CWE-22.html', profile=profile_data)
