baad11d19957bfd1a1602cf895e6fb332d9bf656
SWGO-1819 <tonytony1714@gmail.com>
2025-12-20 17:31:10 +0900

    A02 update

--- route/A02.py
+++ route/A02.py
@@ -23,3 +23,21 @@ def a02_index():
         current_app.logger.error(f"Error decoding JSON from {json_path}")
 
     return render_template('A02/A02.html', cwes=cwe_data)
+
+@a02_bp.route('/cwe-5', methods=['GET', 'POST'])
+def cwe_5():
+    """
+    CWE-22 (Path Traversal) 예시 페이지를 보여줍니다.
+    """
+    # A01.json 파일에서 CWE 데이터를 읽어옵니다.
+    json_path = os.path.join(current_app.root_path, 'data', 'A02.json')
+    cwe_data = []
+    try:
+        with open(json_path, 'r', encoding='utf-8') as f:
+            cwe_data = json.load(f)
+    except FileNotFoundError:
+        current_app.logger.error(f"A01.json not found at {json_path}")
+    except json.JSONDecodeError:
+        current_app.logger.error(f"Error decoding JSON from {json_path}")
+
+    return render_template('A02/CWE-5.html', cwes=cwe_data)
